cbfa98a293e1af9789ba179fe2c971a63d774a16
Chumi90 <abel_chumillas@hotmail.com>
2025-04-24 23:50:22 +0200

    Second Commit correct error in data internal port

--- networkModule.js
+++ networkModule.js
@@ -5,15 +5,17 @@ Dentro de cada interfaz habrá que sacar la Familia, dirección e Interno
 const os = require('node:os');
 const ifaces=os.networkInterfaces()
 
+//console.log(ifaces);
+
 function netWork(){
     console.log("\n");
     Object.keys(ifaces).forEach((value)=>{
         //console.log(ifaces[value]);
         console.log(value);
-        ifaces[value].map((i)=>{
+        ifaces[value].map((i)=>{ //De cada uno de los objetos contenidos por el array de los puertos de interfaces ethernet donde se alojan objetos obenemos su familia, dirección e internal 
                 console.log("familia: "+i.family);
                 console.log("direccion:"+i.address);
-                console.log("interno: "+i.family);
+                console.log("interno: "+i.internal);
         })
         console.log("\n");
     })
